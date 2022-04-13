fcbbf97f77324a61ae246b6045b15144fd849076
Guillem <guillem@disedit.com>
2022-04-13 11:49:26 +0200

    Fix issue with marquee

--- src/components/ConferenceInfo.vue
+++ src/components/ConferenceInfo.vue
@@ -1,21 +1,17 @@
 <template>
   <footer class="conference-info">
-    <Vue3Marquee :duration="30">
+    <MarqueeText :duration="20" :repeat="4">
       <div class="scroll">
         <span class="scroll-title">RESPOSTES VERDES EN TEMPS D'EMERGÈNCIES</span>
-        <span class="scroll-date">7 DE MAIG</span>
-        <span class="scroll-venue">10:00H. COMPLEX CULTURAL LA PETXINA. VALÈNCIA</span>
-        <span class="scroll-title">RESPOSTES VERDES EN TEMPS D'EMERGÈNCIES</span>
-        <span class="scroll-date">7 DE MAIG</span>
-        <span class="scroll-venue">10:00H. COMPLEX CULTURAL LA PETXINA. VALÈNCIA</span>
+        <span class="scroll-date">7 DE MAIG 10:00H</span>
+        <span class="scroll-venue">COMPLEX CULTURAL LA PETXINA. VALÈNCIA</span>
       </div>
-    </Vue3Marquee>
+    </MarqueeText>
   </footer>
 </template>
 
 <script setup>
-import { Vue3Marquee } from 'vue3-marquee'
-import 'vue3-marquee/dist/style.css'
+import MarqueeText from 'vue-marquee-text-component'
 </script>
 
 <style lang="scss" scoped>
@@ -38,47 +34,33 @@ import 'vue3-marquee/dist/style.css'
   gap: 2rem;
 
   &-title {
-    animation: flash-pine 3s steps(4, end) infinite;
+    --to-color: var(--pine);
+    animation: flash 3s infinite;
   }
 
   &-date {
-    animation: flash 3s steps(4, end) infinite;
+    animation: flash 3s infinite;
     animation-delay: 1s;
   }
 
   &-venue {
-    animation: flash 3s steps(4, end) infinite;
+    animation: flash 3s infinite;
     animation-delay: 2s;
   }
 }
 
 @keyframes flash {
   0% {
-    color: var(--green);
+    color: var(--to-color, var(--green));
   }
   15% {
     color: var(--white);
   }
   33% {
-    color: var(--green);
+    color: var(--to-color, var(--green));
   }
   100% {
-    color: var(--green);
-  }
-}
-
-@keyframes flash-pine {
-  0% {
-    color: var(--pine);
-  }
-  15% {
-    color: var(--white);
-  }
-  33% {
-    color: var(--pine);
-  }
-  100% {
-    color: var(--pine);
+    color: var(--to-color, var(--green));
   }
 }
 </style>
